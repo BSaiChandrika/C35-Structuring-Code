0a569f809c55ca80a4d0576c27679d2937cab0ce
BSaiChandrika <69899033+BSaiChandrika@users.noreply.github.com>
2020-09-30 17:36:49 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# C35-Structuring-Code
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database,playerCount;
+var form,player,game;
+
+var gameState=0;
+
+function setup(){
+    database=firebase.database();
+    createCanvas(500,500);
+
+    game=new Game();
+    game.getState();
+    game.start();
+    
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+  
+}
+
